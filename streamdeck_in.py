d65e4cfead6a40c64b5fd739d78ee5e9c6eb4aa3
jp-Notiva <152994533+jp-Notiva@users.noreply.github.com>
2025-03-21 13:49:38 +0100

    Update streamdeck_in.py

--- streamdeck_in.py
+++ streamdeck_in.py
@@ -57,7 +57,7 @@ def button_pressed(streamdeck, key, state):
                 curret_wd = os.path.dirname(os.path.realpath(__file__))
                 repo = git.Repo(curret_wd)
                 repo.head.reset('HEAD', index=True, working_tree=True)
-                repo.remotes.origin.pull()
+                repo.remotes.origin.pull("main")
                 if len(threading.enumerate()) >= 3: #Wait for all threads to finish before loading next menu
                     for thread in threading.enumerate():
                         if thread.getName()[-13:] == "(alert_timer)":
