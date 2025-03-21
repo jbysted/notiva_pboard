f3988a74527e87f384d461877d50ec9aa3ab6804
jp-Notiva <152994533+jp-Notiva@users.noreply.github.com>
2025-03-21 13:43:21 +0100

    Update streamdeck_in.py

--- streamdeck_in.py
+++ streamdeck_in.py
@@ -54,7 +54,9 @@ def button_pressed(streamdeck, key, state):
                 thread = Thread(target=out.alert_timer, args = (deck, key,"Loading")) #Alert the user
                 thread.start()
                 curret_wd = os.path.dirname(os.path.realpath(__file__))
-                repo = git.repo(curret_wd)
+                curret_wd = os.path.dirname(os.path.realpath(__file__))
+                repo = git.Repo(curret_wd)
+                repo.head.reset('HEAD', index=True, working_tree=True)
                 repo.remotes.origin.pull()
                 if len(threading.enumerate()) >= 3: #Wait for all threads to finish before loading next menu
                     for thread in threading.enumerate():
